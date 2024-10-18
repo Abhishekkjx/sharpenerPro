1ead66034a86ae085a3725fce6f57da3f103bee7
Abhishekkjx <abhishekkv1998lko@gmail.com>
2024-10-18 20:28:09 +0530

    FirstTask Adding

---
+++ FirstTask/index.js
@@ -0,0 +1,14 @@
+const header = document.getElementById("header");
+const heading = document.getElementById("main-heading");
+const basketHeading = document.getElementById("basket-heading");
+const pThanks = document.getElementById("thanks");
+
+heading.textContent = "Fruit World";
+heading.style.color = "orange";
+
+header.style.backgroundColor = "green";
+header.style.borderBottom = "3px solid orange";
+
+basketHeading.style.color = "green";
+
+pThanks.textContent = "Please visit us again"
